bd20755c6da95bb9144578b86052291ed41fa08c
Alexey Shuvalov <alexey.shuvalov@hotmail.com>
2015-03-31 15:25:02 +0300

    Moved print method to utility class.

--- src/com/epam/alexey_shuvalov/java/lesson4/task1/SaladChef.java
+++ src/com/epam/alexey_shuvalov/java/lesson4/task1/SaladChef.java
@@ -20,34 +20,20 @@ public class SaladChef {
         Tomato tomato = new Tomato();     
         
         CookBook salad = new Salad(new CulinaryVegetable[]{cucumber, onion, potato, tomato});
-        
-        System.out.println("There are following vegetables in this salad: \n");
-        SaladUtils.showIngredients(salad.getIngredients());
-        System.out.println("\nTotal Calories in this salad: " + salad.countCalories() + " kcal.");
-        System.out.println("\nList of the vegetables sorted by Calories (ascending order): \n");
-        SaladUtils.showIngredients(salad.sortVegetablesByCalories());
-        System.out.println("\nThis salad contains the following vegetables (within 30..-75 Calories range):\n");
+
         try {
-            SaladUtils.showIngredients(salad.findVegetablesByCaloriesRange(30, -75));
+            SaladUtils.printResults(salad, salad.getIngredients(), 30, -75);    
         } catch (CookBookException ex) {
             System.out.println(ex.getMessage());
         }
-
+        
         Salad newSalad = new Salad();
         /* showing an example with null-checks implemented */
-        CulinaryVegetable[] ingredientsForNewSalad = newSalad.getIngredients();
         newSalad.addIngredient(tomato);
         newSalad.addIngredient(onion);
         newSalad.addIngredient(potato);
         newSalad.addIngredient(cucumber);
         
-        System.out.println("\nThere are following vegetables in this salad: \n");
-        SaladUtils.showIngredients(ingredientsForNewSalad);
-        System.out.println("\nTotal Calories in this salad: " + newSalad.countCalories() + " kcal.");
-        System.out.println("\nList of the vegetables sorted by Calories (ascending order): \n");
-        SaladUtils.showIngredients(newSalad.sortVegetablesByCalories());
-        System.out.println("\nThis salad contains the following vegetables (within -30..75 Calories range):\n");
-        SaladUtils.showIngredients(newSalad.findVegetablesByCaloriesRange(-30, 75));
-    } 
-    
+        SaladUtils.printResults(newSalad, newSalad.getIngredients(), -30, 75);
+    }   
 }
--- src/com/epam/alexey_shuvalov/java/lesson4/task1/SaladUtils.java
+++ src/com/epam/alexey_shuvalov/java/lesson4/task1/SaladUtils.java
@@ -1,5 +1,7 @@
 package com.epam.alexey_shuvalov.java.lesson4.task1;
 
+import com.epam.alexey_shuvalov.java.lesson4.task1.model.CookBook;
+import com.epam.alexey_shuvalov.java.lesson4.task1.model.CookBookException;
 import com.epam.alexey_shuvalov.java.lesson4.task1.model.CulinaryVegetable;
 
 /**
@@ -12,5 +14,14 @@ public class SaladUtils {
             if (aCulinaryVegetable != null) System.out.println(aCulinaryVegetable);
         }
     }
-
+    
+     public static void printResults(CookBook salad, CulinaryVegetable[] culinaryVegetables, double min, double max) throws CookBookException {
+        System.out.println("\nThere are following vegetables in this salad: \n");
+        showIngredients(culinaryVegetables);
+        System.out.println("\nTotal Calories in this salad: " + salad.countCalories() + " kcal.");
+        System.out.println("\nList of the vegetables sorted by Calories (ascending order): \n");
+        showIngredients(salad.sortVegetablesByCalories());
+        System.out.println("\nThis salad contains the following vegetables (within -30..75 Calories range):\n");
+        showIngredients(salad.findVegetablesByCaloriesRange(min, max));
+    }
 }
